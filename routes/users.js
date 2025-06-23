b9eb15bafd3d84c6fe151edc28222863eaf54aab
iiqbalh <iqbalhernadi17@gmail.com>
2025-06-23 10:28:15 +0700

    users done

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,115 @@
 var express = require('express');
 var router = express.Router();
 const { User } = require('../models');
+const { Op } = require('sequelize');
 const { isLoggedIn } = require('../helper/util');
 
 /* GET users listing. */
 router.get('/', isLoggedIn, async function (req, res, next) {
   try {
-    const users = await User.findAll();
-    // res.status(200).json(users[0])
-    res.render('users', {
+    let { page = 1, query = '', sortBy = 'id', sortMode = 'asc', limit = 3 } = req.query
+    const url = req.url == '/' ? `/?page=${page}&sortBy=${sortBy}&sortMode=${sortMode}` : req.url;
+
+    const params = {};
+
+    //searching
+    if (query) {
+      params['where'] = {
+        ...params.where,
+        name: {
+          [Op.iLike]: `%${query}%`
+        }
+      }
+    }
+
+    //pagination
+    limit = Number(limit)
+    const offset = limit * (page - 1);
+    const count = await User.count(params);
+    const pages = Math.ceil(count / limit)
+
+    //sorting
+    params['order'] = [
+      [sortBy, sortMode == 'asc' ? 'ASC' : 'DESC']
+    ]
+
+    params['limit'] = limit
+    params['offset'] = offset
+
+    const users = await User.findAll(params);
+    // res.status(200).json(users[0], page, pages, limit, offset)
+    res.render('users/list', {
+      data: users,
       title: 'users',
-      name: req.session.name
+      name: req.session.name,
+      page,
+      count,
+      pages,
+      sortBy,
+      sortMode,
+      offset,
+      limit,
+      url
     });
   } catch (e) {
     res.status(500).json({ message: e.message })
   }
 });
 
+router.get('/add', isLoggedIn, async function (req, res, next) {
+  try {
+    // res.status(200).json(users[0])
+    res.render('users/add', {});
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.post('/add', async function (req, res, next) {
+  try {
+    const { email, name, password, role } = req.body;
+    const user = await User.create({ email, name, password, role })
+    res.redirect('/users');
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get('/edit/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      console.log('User not found');
+    }
+
+    res.render('users/edit', {
+      data: user
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.post('/edit/:id', async (req, res, next) => {
+  try {
+    const { email, name, role } = req.body;
+    const { id } = req.params;
+
+    const [updatedRows] = await User.update({ email, name, role }, { where: { id } });
+
+    if (updatedRows === 0) {
+      console.log('error', 'User not found');
+    } else {
+      console.log('success', 'User updated successfully');
+    }
+
+    res.redirect('/users');
+
+  } catch (err) {
+    console.error(err);
+    res.redirect('/users');
+  }
+});
 
 module.exports = router;
